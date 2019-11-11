1c74bd41dcbed0bb6fa5d3dac8e426d12f58c2d4
Mikhail Burilov <burilovmv@gmail.com>
2019-11-11 18:37:56 +0300

    Added test harness config, fixed gitlab service ip address

--- run-tests.sh
+++ run-tests.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+
+IPADDR=$(hostname -i)
+
 docker run -d --rm --name kudu -p 8050:8050 -p 8051:8051 -p 7050:7050 -p 7051:7051 \
--e KUDU_MASTER_EXTRA_OPTS='--webserver_advertised_addresses localhost:8051 --rpc_advertised_addresses localhost:7051' \
--e KUDU_TSERVER_EXTRA_OPTS='--webserver_advertised_addresses localhost:8050 --rpc_advertised_addresses localhost:7050' \
+-e KUDU_MASTER_EXTRA_OPTS='--webserver_advertised_addresses $IPADDR:8051 --rpc_advertised_addresses $IPADDR:7051' \
+-e KUDU_TSERVER_EXTRA_OPTS='--webserver_advertised_addresses $IPADDR:8050 --rpc_advertised_addresses $IPADDR:7050' \
 s2shape/kudu-dev
 
 sleep 10
 
-export KUDU_HOST=localhost
+export KUDU_HOST=$IPADDR
 export KUDU_PORT=7051
 
 dotnet restore -s https://www.myget.org/F/s2/ -s https://api.nuget.org/v3/index.json
